e88ca1dfd006cf435a8370bea269a6344043c057
unknown <apcrash@gmail.com>
2014-03-21 02:48:35 +0800

    Moved vendor config to autoload.

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,15 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'BjyProfiler',
+        'ZendDeveloperTools',
+        'ZFTool',
+        'DoctrineModule',
+        'DoctrineORMModule',
+        'PhlySimplePage',
+        'PhlyContact',
+        'EdpModuleLayouts',
+        'SpeckPaypal',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
