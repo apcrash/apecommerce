99ce288fb27e53283557099c36f0733a246fdd78
unknown <apcrash@gmail.com>
2014-03-25 10:44:31 +0800

    Added AsseticBundle Module in config.

--- config/application.config.php
+++ config/application.config.php
@@ -18,6 +18,7 @@ return array(
         'ZfcUserDoctrineORM',
         'ApeUser',
         'Core',
+        'AsseticBundle',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
